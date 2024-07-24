99c0c529d601c777177444356308d5b0b8bbdaaf
Noah Witzke <noah.witzke@icloud.com>
2024-07-24 03:56:39 -0400

    Initial Commit

---
+++ Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by Noah Witzke on 2024-07-24.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Application
+//
+//  Created by Noah Witzke on 2024-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
